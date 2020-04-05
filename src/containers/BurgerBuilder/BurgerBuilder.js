16176a711783e827448bd6562d760b9858a4de0d
AlanAlv <alan.alv96@gmail.com>
2020-04-05 15:47:16 -0600

    Reducer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,13 +26,13 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
-        axios.get('https://react-my-burger-23a26.firebaseio.com/ingredients.json')
+/*         axios.get('https://react-my-burger-23a26.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
             })
             .catch(error => {
                 this.setState({error: true})
-            });
+            }); */
     };
 
     updatePurchaseState (ingredients) {
