de0e15b26f56f92f00477cc4d93c73743a32196c
AlanAlv <alan.alv96@gmail.com>
2020-04-07 15:30:06 -0500

    Combine reducers

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -9,7 +9,6 @@ export const purchaseBurgerSuccess = (id, orderData) => {
     };
 };
 
-
 export const purchaseBurgerFail = (error) => {
     return {
         type: actionTypes.PURCHASE_BURGER_FAILED,
@@ -28,7 +27,7 @@ export const purchaseBurger = (orderData) => {
         axios.post('/orders.json', orderData)
         .then(response => {
             console.log(response.data);
-            dispatch(purchaseBurgerSuccess(response.data, orderData));
+            dispatch(purchaseBurgerSuccess(response.data.name, orderData));
         })
         .catch(error => {
             dispatch(purchaseBurgerFail(error));
