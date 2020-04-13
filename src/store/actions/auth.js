3ead0b0d0fecd06ae8963eb0318d46975c8fa62e
AlanAlv <alan.alv96@gmail.com>
2020-04-13 15:54:50 -0500

    Sign up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAUb1t6qNoA5S28h7mGrEz11oAhe7q6D7I', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSucess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
+        ;
     };
 };
