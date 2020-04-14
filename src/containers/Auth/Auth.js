d6778cacd24ef14035f373b3a02d268952ac6e72
AlanAlv <alan.alv96@gmail.com>
2020-04-14 13:57:18 -0500

    Storing token

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -87,7 +87,7 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup.value)
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup)
     }
 
     switchAuthModeHandler = () => {
